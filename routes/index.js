eb322be85a8f9d27270e6105b99c8606e8569464
ankitkicode <aj0353342@gmail.com>
2024-09-18 16:44:04 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+  
 });
 
 router.get('/home', isloggedIn, function (req, res, next) {
